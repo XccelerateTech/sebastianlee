e39142dc169627e1c5dfd4dc204636cc9009634d
Sebastian Lee <leepakwing2006@yahoo.com.hk>
2019-03-21 18:28:31 +0800

    day14 stuff

---
+++ web/day14/main.js
@@ -0,0 +1,11 @@
+{/* <canvas id="canvas">Your browser does not support Canvas</canvas>
+<script type='text/javascript'>
+    $(function(){
+        const canvas = document.getElementById('canvas');
+        const ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.moveTo(20,20);
+        ctx.lineTo(200,200);
+        ctx.stroke();
+    })
+</script> */}
